998c69640bee8380622b32d1f379d17cc6125716
Adam Giese <adam.giese@gmail.com>
2016-02-26 22:03:42 -0600

    Initial archive template

---
+++ archive.php
@@ -0,0 +1,16 @@
+<?php get_header(); ?>
+<header class="page-header">
+  <?php the_archive_title( '<h1 class="page-header-title">', '</h1>' ); ?>
+</header>
+<main class="page-content">
+  <?php if ( have_posts() ) { ?>
+    <?php while ( have_posts() ) {
+      the_post(); ?>
+      <article class="page-article">
+        <h2 class="page-article-title"><?php the_title(); ?></h2>
+        <span class="page-article-date"><?php echo get_the_date(); ?></span>
+      </article>
+    <?php } ?>
+  <?php } ?>
+</main>
+<?php get_footer(); ?>
