692ded8828af0bffbe4dab187a60bb251a7df68c
Adam Giese <adam.giese@gmail.com>
2016-02-27 15:44:19 -0600

    Add footer and copyright

--- footer.php
+++ footer.php
@@ -1,4 +1,5 @@
-<footer>
+<footer class="site-footer">
+  <div class="site-footer-copyright">&copy; <?php echo date("Y"); ?>  <?php echo get_bloginfo('name'); ?></div>
 </footer>
 <?php wp_footer(); ?>
 </body>
