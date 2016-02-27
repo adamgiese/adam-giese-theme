8a5bc92119ab561160b425391b1b8e9e769147f1
Adam Giese <adam.giese@gmail.com>
2016-02-27 15:25:41 -0600

    Add default template

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
 <?php get_header(); ?>
-<main></main>
+<header class="page-header">
+  <?php if ( ! is_front_page() && ! is_home() ) { ?>
+    <h1 class="page-header-title"><?php the_title(); ?></h1>
+  <?php } ?>
+</header>
+<main class="page-content">
+  <?php if ( have_posts() ) { while ( have_posts() ) { the_post(); ?>
+  <article class="page-article">
+    <?php the_content(); ?>
+  </article>
+  <?php } } ?>
+</main>
 <?php get_footer(); ?>
