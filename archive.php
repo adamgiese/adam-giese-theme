43131f9e752e54c6fa2107582587af4f79a93589
Adam Giese <adam.giese@gmail.com>
2016-02-27 15:30:47 -0600

    Condense The Loop into one line

--- archive.php
+++ archive.php
@@ -3,14 +3,11 @@
   <?php the_archive_title( '<h1 class="page-header-title">', '</h1>' ); ?>
 </header>
 <main class="page-content">
-  <?php if ( have_posts() ) { ?>
-    <?php while ( have_posts() ) {
-      the_post(); ?>
+  <?php if ( have_posts() ) { while ( have_posts() ) { the_post(); ?>
       <article class="page-article">
         <h2 class="page-article-title"><?php the_title(); ?></h2>
         <span class="page-article-date"><?php echo get_the_date(); ?></span>
       </article>
-    <?php } ?>
-  <?php } ?>
+    <?php } } ?>
 </main>
 <?php get_footer(); ?>
