481c9327bb9bf0cf07517e2457e0a34e96f332b1
Adam Giese <adam.giese@gmail.com>
2016-02-28 13:13:07 -0600

    Add permalinks to archive template

--- archive.php
+++ archive.php
@@ -5,7 +5,7 @@
 <main class="page-content">
   <?php if ( have_posts() ) { while ( have_posts() ) { the_post(); ?>
       <article class="page-article">
-        <h2 class="page-article-title"><?php the_title(); ?></h2>
+        <h2 class="page-article-title"><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h2>
         <span class="page-article-date"><?php echo get_the_date(); ?></span>
       </article>
     <?php } } ?>
